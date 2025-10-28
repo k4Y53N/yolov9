46d467d2355b52c8cfe592d10a6bf0cf560267c5
2306009 <2306009@iisigroup.com>
2025-10-28 11:38:47 +0800

    add --u8-head uint8 head support

--- update_dim.py
+++ update_dim.py
@@ -6,46 +6,85 @@ from onnx.tools.update_model_dims import update_inputs_outputs_dims
 from onnxconverter_common.float16 import convert_float_to_float16
 
 
-def add_cast_fp16_layer(model: onnx.ModelProto):
-    # Assume the first node is the input layer and we want to remove it
-    # In ONNX, rather than "removing" an input layer, you would adjust the graph to change inputs
-
-    # Create a Cast node to cast input from float32 to float16
-    # Note: You need to know the input name and shape
-    input_name = model.graph.input[0].name
-    output_name = 'cast_0'
-    cast_node = helper.make_node(
-        'Cast', inputs=[input_name], outputs=[output_name], to=TensorProto.FLOAT16
-    )
-
-    # Modify the graph's first node to take the output of the cast node as its input
-    model.graph.node[0].input[0] = output_name
-
-    # Add the cast node to the beginning of the graph
-    model.graph.node.insert(0, cast_node)
-
-    return model
-
-
-def fp32_to_fp16_with_cast(model: onnx.ModelProto, size: int, batch_name: str):
-    add_cast_fp16_layer(model)
+def update_input_tensor_dtype(
+    model: onnx.ModelProto,
+    dtype: int,
+    batch_name: str,
+    size: int,
+):
     input_name = model.graph.input[0].name
     new_input_tensor = helper.make_tensor_value_info(
-        input_name,  # Name of the new input
-        TensorProto.FLOAT,  # Data type
+        input_name,
+        dtype,
         [batch_name, 3, size, size],  # Shape: [batch, channels, height, width]
     )
-    # Replace the original input with the new one
-    # Remove the original input
     model.graph.input.pop(0)
-    # Add the new input
     model.graph.input.insert(0, new_input_tensor)
 
     return model
 
 
+def insert_cast(model: onnx.ModelProto, dtype: int):
+    input_name = model.graph.input[0].name
+    cast_output = 'cast_0'
+    cast_node = helper.make_node(
+        'Cast',
+        inputs=[input_name],
+        outputs=[cast_output],
+        to=dtype,
+    )
+    model.graph.node[0].input[0] = cast_output
+    model.graph.node.insert(0, cast_node)
+
+    return model
+
+
+def insert_cast_divide_255(model: onnx.ModelProto, dtype: int):
+    input_name = model.graph.input[0].name
+    cast_output = 'cast_0'
+    div_output = 'div_255_output'
+    cast_node = helper.make_node(
+        'Cast',
+        inputs=[input_name],
+        outputs=[cast_output],
+        to=dtype,
+    )
+    const_name = 'const_255'
+    const_tensor = helper.make_tensor(const_name, dtype, [], [255.0])
+    model.graph.initializer.append(const_tensor)
+    div_node = helper.make_node(
+        'Div',
+        inputs=[cast_output, const_name],
+        outputs=[div_output],
+    )
+    model.graph.node[0].input[0] = div_output
+    model.graph.node.insert(0, div_node)
+    model.graph.node.insert(0, cast_node)
+
+    return model
+
+
 def main(args):
+    print(f'Loading model from {args.input}...')
+
+    if args.u8_head and args.no_cast:
+        print('--u8-head with force create cast layer, --no-cast is ignored.')
+
     model = onnx.load(args.input)
+    dtype = TensorProto.FLOAT
+
+    if args.half:
+        model = convert_float_to_float16(model)
+        dtype = TensorProto.FLOAT16
+
+    if args.u8_head:
+        insert_cast_divide_255(model, dtype=dtype)
+        dtype = TensorProto.UINT8
+    elif args.half and not args.no_cast:
+        # If model is converted to FP16, add Cast to FLOAT input
+        insert_cast(model, dtype=dtype)
+        dtype = TensorProto.FLOAT
+
     output_node = model.graph.output
     model = update_inputs_outputs_dims(
         model,
@@ -55,13 +94,7 @@ def main(args):
             for node in output_node
         },
     )
-
-    if args.half:
-        model = convert_float_to_float16(model)
-
-        if not args.no_cast:
-            fp32_to_fp16_with_cast(model, args.size, args.batch_name)
-
+    update_input_tensor_dtype(model, dtype, args.batch_name, args.size)
     onnx.save(model, args.output)
     print(f'Saved updated model to {args.output}')
 
@@ -92,6 +125,11 @@ if __name__ == '__main__':
         required=False,
         default='batch',
         help='batch dim alias name, default `batch`',
+    )
+    parser.add_argument(
+        '--u8-head',
+        action='store_true',
+        help='add uint8 input head with cast and division by 255',
     )
 
     args = parser.parse_args()
